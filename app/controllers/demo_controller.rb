1e6b8f82919b449bc96d5682f20fd9e37bba19e1
jro02202 <j.rojas@baypath.net>
2018-09-18 22:09:34 +0000

    Back to Normal

--- app/controllers/demo_controller.rb
+++
@@ -1,4 +0,0 @@
-class DemoController < ApplicationController
-  def yo
-  end
-end
--- test/controllers/demo_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class DemoControllerTest < ActionDispatch::IntegrationTest
-  test "should get yo" do
-    get demo_yo_url
-    assert_response :success
-  end
-
-end
